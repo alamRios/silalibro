1bbd38daf5cf97e97efb6c75180d96d5ba526c15
alamRios <alam.rios@ymail.com>
2018-05-31 13:44:38 -0500

    Datos de usuario

--- src/main/java/com/silalibro/web/bean/UserBean.java
+++ src/main/java/com/silalibro/web/bean/UserBean.java
@@ -14,69 +14,81 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+
 /**
  *
  * @author ariosa1500
  */
 @ManagedBean
 @SessionScoped
-public class UserBean implements Serializable{
+public class UserBean implements Serializable {
+
     /* DAO */
-    private UsuarioDAO usuarioDAO_; 
-    
+    private UsuarioDAO usuarioDAO_;
+
     /* GLOBAL VARIABLES */
-    private String mensaje; 
-    private UsuarioDTO usuario; 
-    
+    private String mensaje;
+    private UsuarioDTO usuario;
+
     /* LOGIN VARIABLES */
     private String correo_usr;
     private String passwd_usr;
-    private boolean credencialesIncorrectas; 
-    
-    public UserBean(){
+    private boolean credencialesIncorrectas;
+
+    public UserBean() {
     }
-    
+
     @PostConstruct
-    public void setup()  {
-        mensaje = "Bienvenido a Silalibro"; 
-        credencialesIncorrectas = false; 
-        usuarioDAO_ = new UsuarioDAO(); 
+    public void setup() {
+        mensaje = "Bienvenido a Silalibro";
+        credencialesIncorrectas = false;
+        usuarioDAO_ = new UsuarioDAO();
+        Integer idusuario = (Integer) FacesContext.getCurrentInstance()
+                .getExternalContext().getSessionMap().get("idusuario");
+        try {
+            if (idusuario == null) {
+                ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+                context.getFlash().setKeepMessages(true);
+                context.redirect(context.getRequestContextPath() + "/");
+            }
+        } catch (Exception ex) {
+        }
     }
-        
-    public void cerrarSesion(){
-        try{
+
+    public void cerrarSesion() {
+        try {
             FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
             FacesContext.getCurrentInstance().getExternalContext().redirect("/silalibro");
-        }catch(Exception ex){
-            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_WARN, 
+        } catch (Exception ex) {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_WARN,
                     "Ha ocurrido un error",
                     ex.toString());
             FacesContext.getCurrentInstance().addMessage(null, msg);
         }
     }
-    
-    public void iniciarSesion(){
-        try{
-            usuario = usuarioDAO_.iniciarSesion(correo_usr,passwd_usr); 
-            if(usuario != null){
+
+    public void iniciarSesion() {
+        try {
+            usuario = usuarioDAO_.iniciarSesion(correo_usr, passwd_usr);
+            if (usuario != null) {
                 FacesContext.getCurrentInstance()
                         .getExternalContext().getSessionMap().put("idusuario", usuario.getIdusuario());
-                ExternalContext context; 
-                if(usuario.isAdministrador()){
+                ExternalContext context;
+                if (usuario.isAdministrador()) {
                     context = FacesContext.getCurrentInstance().getExternalContext();
                     context.getFlash().setKeepMessages(true);
                     context.redirect(context.getRequestContextPath() + "/administrador/index.xhtml");
                 }
-            }else{
-                credencialesIncorrectas = true; 
-                FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_WARN, 
-                    "Ha ocurrido un error",
-                    "Credenciales incorrectas");
+            } else {
+                credencialesIncorrectas = true;
+                FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_WARN,
+                        "Ha ocurrido un error",
+                        "Credenciales incorrectas");
                 FacesContext.getCurrentInstance().addMessage(null, msg);
             }
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
-            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_WARN, 
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_WARN,
                     "Ha ocurrido un error",
                     ex.toString());
             FacesContext.getCurrentInstance().addMessage(null, msg);
@@ -122,6 +134,5 @@ public class UserBean implements Serializable{
     public void setCredencialesIncorrectas(boolean credencialesIncorrectas) {
         this.credencialesIncorrectas = credencialesIncorrectas;
     }
-    
-    
+
 }

c36e119df5d297aca4cd9e28e9e373efb5e0f37c
alamRios <alam.rios@ymail.com>
2018-06-01 17:05:12 -0500

    Autores

---
+++ src/main/java/com/silalibro/dto/AutorDTO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.silalibro.dto;
+
+/**
+ *
+ * @author arios
+ */
+public class AutorDTO {
+    public String nombre; 
+    public String apellidoPaterno; 
+    public String appellidoMaterno; 
+    public String nombrePais; 
+    public int idPais; 
+}
---
+++ src/main/java/com/silalibro/web/bean/AutorBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.silalibro.web.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author arios
+ */
+@ManagedBean
+@ViewScoped
+public class AutorBean {
+    
+}

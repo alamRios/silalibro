a6fcad42110be0da8f590224758c3c15a6cdccc6
boozinas <boozmedina@gmail.com>
2018-06-05 16:06:17 -0500

    Registro de Libros_popo

--- src/main/java/com/silalibro/dao/LibroDAO.java
+++ src/main/java/com/silalibro/dao/LibroDAO.java
@@ -26,115 +26,120 @@ import org.apache.commons.io.FileUtils;
  * @author boozh
  */
 public class LibroDAO {
-    private final String SQL_SELECT_REGISTRAR_LIBRO_PORTADA = "INSERT INTO libro (libro_col) values (?);";
+
     private final String SQL_SELECT_ID = "SELECT * from libro where idlibro=?;";
     private final String SQL_SELECT_LIBROS_DISPONIBLES = "select * from libro join autor on libro_idautor = idautor join pais on autor_idpais = idpais;";
     private final String SQL_SELECT_CATEGORIA = "SELECT * from libro where libro_categoria=?;";
-    private final String SQL_INSERT_REGISTRAR_LIBRO = "INSERT INTO libro (libro_sku,libro_titulo,libro_idautor, libro_categoria) VALUES (?,?,?,?);"; 
-        
-    public boolean registrarLibro(LibroDTO libro) throws Exception{
-        Connection con = null; 
-        PreparedStatement st = null; 
-        try{
-            con = Conection.obtenerConeccion(); 
-            st = con.prepareStatement(SQL_INSERT_REGISTRAR_LIBRO); 
+    private final String SQL_INSERT_REGISTRAR_LIBRO = "INSERT INTO libro (libro_sku,libro_titulo,libro_idautor, libro_categoria, librocol) VALUES (?,?,?,?,?);";
+
+    public boolean registrarLibro(LibroDTO libro) throws Exception {
+        Connection con = null;
+        PreparedStatement st = null;
+        try {
+            con = Conection.obtenerConeccion();
+            st = con.prepareStatement(SQL_INSERT_REGISTRAR_LIBRO);
             st.setString(1, libro.getSku());
             st.setString(2, libro.getTitulo());
             st.setInt(3, libro.getIdautor());
             st.setString(4, libro.getCategoria());
             ResultSet rs = st.executeQuery();
-            if(rs.next()){
-                        try(InputStream input = libro.getLibrocol().getInputStream()){
-                            String imgpath = "/resources/portadas/";
-                            if (!Files.exists(Paths.get(imgpath)))
-                                Files.createDirectories(Paths.get(imgpath));
-                            imgpath += rs.getLong(1)+".jpg";
-                            File destFile = new File(imgpath);
-                            try {                    
-                                FileUtils.copyInputStreamToFile(input, destFile);
-                            } catch (Exception e) {
-                                throw e; 
-                            }
-                            st = con.prepareStatement(SQL_SELECT_REGISTRAR_LIBRO_PORTADA);
-                            st.setString(1, imgpath+rs.getLong(1));
-                            return st.executeUpdate()>0; }
-                        catch(Exception ex){
-                            throw ex; 
-                        }}
-        }catch(Exception ex){
+            if (rs.next()) {
+                try (InputStream input = libro.getLibrocol().getInputStream()) {
+                    String imgpath = "resources/portadas/";
+                    if (!Files.exists(Paths.get(imgpath))) {
+                        Files.createDirectories(Paths.get("../"+imgpath));
+                    }
+                    imgpath += rs.getLong(1) + ".jpg";
+                    File destFile = new File(imgpath);
+                    try {
+                        FileUtils.copyInputStreamToFile(input, destFile);
+                    } catch (Exception e) {
+                        throw e;
+                    }
+                    
+                    st.setString(5, imgpath + rs.getLong(1));
+                    return st.executeUpdate() > 0;
+                } catch (Exception ex) {
+                    throw ex;
+                }
+            }
+        } catch (Exception ex) {
             System.out.println(ex);
-            throw ex; 
-        }finally{
-            try{
-                if(con != null)
+            throw ex;
+        } finally {
+            try {
+                if (con != null) {
                     con.close();
-                if(st != null)
+                }
+                if (st != null) {
                     st.close();
-                
-            }catch(Exception ex){
-                throw ex; 
+                }
+
+            } catch (Exception ex) {
+                throw ex;
             }
         }
-        return true; 
+        return true;
     }
-     public boolean LibroPorId(int idlibro) throws Exception{
-        Connection con = null; 
+
+    public boolean LibroPorId(int idlibro) throws Exception {
+        Connection con = null;
         PreparedStatement st = null;
-        ResultSet rs = null; 
-        try{
-            con = Conection.obtenerConeccion(); 
-            st = con.prepareStatement(SQL_SELECT_ID); 
+        ResultSet rs = null;
+        try {
+            con = Conection.obtenerConeccion();
+            st = con.prepareStatement(SQL_SELECT_ID);
             st.setInt(1, idlibro);
             rs = st.executeQuery();
-            if(rs.next()){
-                LibroDTO libro = new LibroDTO(); 
+            if (rs.next()) {
+                LibroDTO libro = new LibroDTO();
                 InputStream is = rs.getBinaryStream(1);
                 OutputStream os = new FileOutputStream(new File("Test.jpg"));
                 is.close();
                 os.close();
             }
-        }catch(Exception ex){
-            throw ex; 
+        } catch (Exception ex) {
+            throw ex;
         }
-        
-     return true;
-     }
-     
-     public boolean LibroPorCategoria(String Categoria) throws Exception{
-        Connection con = null; 
+
+        return true;
+    }
+
+    public boolean LibroPorCategoria(String Categoria) throws Exception {
+        Connection con = null;
         PreparedStatement st = null;
-        ResultSet rs = null; 
-        try{
-            con = Conection.obtenerConeccion(); 
-            st = con.prepareStatement(SQL_SELECT_CATEGORIA); 
+        ResultSet rs = null;
+        try {
+            con = Conection.obtenerConeccion();
+            st = con.prepareStatement(SQL_SELECT_CATEGORIA);
             st.setString(1, Categoria);
             rs = st.executeQuery();
-            if(rs.next()){
-            LibroDTO libro = new LibroDTO(); 
+            if (rs.next()) {
+                LibroDTO libro = new LibroDTO();
                 InputStream is = rs.getBinaryStream(1);
                 OutputStream os = new FileOutputStream(new File("Test.jpg"));
                 is.close();
                 os.close();
             }
-        }catch(Exception ex){
-            throw ex; 
+        } catch (Exception ex) {
+            throw ex;
         }
-        
-        return true;
-     }
 
-    public List<LibroDTO> obtenerLibrosDisponibles() throws Exception{
-        List<LibroDTO> libros = new ArrayList<>(); 
-        Connection con = null; 
+        return true;
+    }
+
+    public List<LibroDTO> obtenerLibrosDisponibles() throws Exception {
+        List<LibroDTO> libros = new ArrayList<>();
+        Connection con = null;
         PreparedStatement st = null;
-        ResultSet rs = null; 
-        try{
-            con = Conection.obtenerConeccion(); 
-            st = con.prepareStatement(SQL_SELECT_LIBROS_DISPONIBLES); 
+        ResultSet rs = null;
+        try {
+            con = Conection.obtenerConeccion();
+            st = con.prepareStatement(SQL_SELECT_LIBROS_DISPONIBLES);
             rs = st.executeQuery();
-            while(rs.next()){
-                LibroDTO libro = new LibroDTO(); 
-                AutorDTO autor = new AutorDTO(); 
+            while (rs.next()) {
+                LibroDTO libro = new LibroDTO();
+                AutorDTO autor = new AutorDTO();
                 autor.setNombrePais(rs.getString("pais_nombre"));
                 autor.setNombre(rs.getString("autor_nombre"));
                 autor.setApellidoPaterno(rs.getString("autor_apellidoPaterno"));
@@ -143,16 +148,15 @@ public class LibroDAO {
                 libro.setRutaLibro(rs.getString("librocol"));
                 libro.setSku(rs.getString("libro_sku"));
                 libro.setTitulo(rs.getString("libro_titulo"));
-                
+
                 libros.add(libro);
             }
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
-            throw ex; 
+            throw ex;
         }
-        
-        return libros; 
-    }
-    
-}
 
+        return libros;
+    }
+
+}
